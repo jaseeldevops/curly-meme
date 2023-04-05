b305cc232cd8cf77204ea915f45b8a3e7e6928e7
jaseeldevops <appcare@APPCAREs-MacBook-Air.local>
2023-04-05 19:21:20 +0530

    ordinery push

--- src/screens/networks.js
+++ src/screens/networks.js
@@ -16,8 +16,17 @@ export default class NetworksScreen extends Component {
     return (
       <StrictMode>
         <div className="nwA">
-          <div className="nwAa">CONTACT US</div>
-          <div className="nwAb">We love to hear from you.</div>
+          <div className="nwAa">
+            <div className="nwAaA">IMPACT INVESTORS</div>
+            <div className="nwAaB">
+              Impact investing has become increasingly popular in recent years,
+              as more investors are looking to align their investments with
+              their values and make a positive impact in the world. Impact
+              investors are powerful force for good, helping to finance
+              businesses and projects that have the potential to create positive
+              change in society and the environment.
+            </div>
+          </div>
         </div>
         <AppFooter state={state} />
         <AppHeader state={state} />
